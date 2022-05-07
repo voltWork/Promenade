d5d4a7509305cbe738d3607e2c77f5df8179b2b5
Ilya Zhidkov <xkelxmc@yandex.ru>
2022-05-07 12:05:03 +0300

    fix

--- src/Screens/Main.js
+++ src/Screens/Main.js
@@ -9,25 +9,26 @@ export const Main = () => {
     const [ChoosenSection, setChoosenSection] = useState();
     const navigation = useNavigation();
     const handleChoose = value => () => {
-        navigation.navigate(SECTION_SCREEN, { value });
+        navigation.navigate(SECTION_SCREEN, { section: value });
     };
     return (
         <View backgroundcolor={'#ffffff'}>
             <Image source={require('./picture/map.png')} />
             <HStack w='100%' space={2} px='2' mt='4'>
-                <Button size='sm' variant='outline' onPress={handleChoose('places')}>
+                <Button size='sm' backgroundColor="#eab308" flex={1} variant='outline' onPress={handleChoose('places')}>
                     Места
                 </Button>
 
-                <Button size='sm' variant='outline' onPress={handleChoose('mushrooms')}>
+                <Button size='sm' backgroundColor="#eab308" flex={1} variant='outline' onPress={handleChoose('mushrooms')}>
                     Грибы
                 </Button>
-
-                <Button size='sm' variant='outline' onPress={handleChoose('animals')}>
+            </HStack>
+            <HStack w='100%' space={2} px='2' mt='4'>
+                <Button size='sm' backgroundColor="#eab308" flex={1} variant='outline' onPress={handleChoose('animals')}>
                     Животные
                 </Button>
 
-                <Button size='sm' variant='outline' onPress={handleChoose('flowers')}>
+                <Button size='sm' backgroundColor="#eab308" flex={1} variant='outline' onPress={handleChoose('flowers')}>
                     Растения
                 </Button>
             </HStack>
